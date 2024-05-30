f0ea5a86b98bbb2c2eb69b5c19a7f2cbd97bce68
A01754650 <88682624+A01754650@users.noreply.github.com>
2024-05-29 22:06:41 -0600

    update

--- src/controllers/CuentaController.ts
+++ src/controllers/CuentaController.ts
@@ -8,71 +8,24 @@ class CuentaController extends AbstractController {
     if (this._instance) {
       return this._instance;
     }
-    this._instance = new CuentaController("Cuenta");
+    this._instance = new CuentaController("cuenta");
     return this._instance;
   }
 
   protected initializeRoutes(): void {
-    this.router.post("/cargarCuentas", this.cargarCuentas.bind(this));
-    this.router.get(
-      "/:idCliente/cuentas",
-      this.getCuentasPorCliente.bind(this)
-    );
+    this.router.post("/cuentas",this.getCuentasPorCliente.bind(this));
   }
 
-  private async cargarCuentas(req: Request, res: Response) {
-    try {
-      const cuentas = req.body;
-      if (!Array.isArray(cuentas)) {
-        return res.status(400).send("Se espera un arreglo de cuentas");
-      }
-  
-      const cuentasCreadas = [];
-      for (const cuenta of cuentas) {
-        const { idCliente, saldo, tipo, numCuenta } = cuenta;
-        if (!idCliente || saldo == null || !tipo || !numCuenta) {
-          return res
-            .status(400)
-            .send("Todos los campos son requeridos para cada cuenta, incluyendo el número de cuenta");
-        }
-  
-        const nuevaCuenta = await db.Cuenta.create({
-          idCliente,
-        });
-  
-        const nuevaTarjeta = await db.Tarjeta.create({
-          idCuenta: nuevaCuenta.idCuenta,
-          numCuenta,  // Asegúrate de pasar el número de cuenta aquí
-          saldo,
-          tipo,
-        });
-  
-        cuentasCreadas.push({ cuenta: nuevaCuenta, tarjeta: nuevaTarjeta });
-      }
-  
-      res.status(201).json(cuentasCreadas);
-    } catch (err) {
-      console.error("Error al cargar cuentas y tarjetas:", err);
-      res.status(500).send("Error al cargar cuentas y tarjetas");
-    }
-  }
+
   
 
   private async getCuentasPorCliente(req: Request, res: Response) {
     try {
-      const { idCliente } = req.params;
-      if (!idCliente) {
-        return res.status(400).send("ID de cliente es requerido");
-      }
+      const idCliente = req.body.idCliente;
 
       const cuentas = await db.Cuenta.findAll({
-        where: { idCliente },
-        include: [
-          { model: db.Tarjeta, as: "Tarjetas" },
-          { model: db.Transaccion, as: "Transacciones" },
-        ],
+        where: { idCliente: idCliente },
       });
-
       res.status(200).json(cuentas);
     } catch (err) {
       console.error("Error al obtener cuentas por cliente:", err);
