68f501dd06ca835bd4e3bf30996a979ccff18d2b
Alonso Segura <alonsoseg26@outlook.com>
2024-06-12 16:48:34 -0600

    últimas modificaciones parte 1

--- src/controllers/CursoController.ts
+++ src/controllers/CursoController.ts
@@ -33,6 +33,21 @@ class CursoController extends AbstractController {
     this.router.get("/especifico", this.getSpecificCurso.bind(this));
   }
 
+  
+  /**
+   * POST -> Crear un nuevo curso
+   * ENDPOINT -> curso/crear
+   * 
+   * PARAMETROS DE REQUEST:
+   * - nombre
+   * - descripcion
+   * - url
+   * - idAreasOportunidad[]
+   * 
+   * SIN DATOS EN RESPONSE
+   * 
+   * 
+   */
   private async postCrear(req: Request, res: Response) {
     try {
       const newCurso = await db.Curso.create(req.body);
@@ -48,6 +63,27 @@ class CursoController extends AbstractController {
     }
   }
 
+  
+  /**
+   * GET -> Obtener cursos asignados a un agente
+   * ENDPOINT -> curso/asignados
+   * 
+   * PARAMETROS DE REQUEST:
+   * - idAgente
+   * 
+   * RESPONSE:
+   * Lista con:
+   * - idCurso
+   * - nombre
+   * - url
+   * - descripcion
+   * - prioridad
+   * - estado
+   * - fecha 
+   * de todos los cursos asignados a ese agente
+   * 
+   * 
+   */
   private async GetAllByAgente(req: Request, res: Response) {
     try {
       const idAgenteTarget = req.query.idAgente;
@@ -87,6 +123,23 @@ class CursoController extends AbstractController {
     }
   }
 
+  /**
+   * GET -> Obtener todos los cursos de una área en específico
+   * ENDPOINT -> curso/porArea
+   * 
+   * PARAMETROS DE REQUEST:
+   * - idArea
+   * 
+   * RESPONSE:
+   * Lista con:
+   * - idCurso
+   * - nombre
+   * - url
+   * - descripcion
+   * de todos los cursos pertenecientes a esa área
+   * 
+   * 
+   */
   private async GetAllByArea(req: Request, res: Response) {
     try {
       const idAreaTarget = req.query.idArea;
@@ -120,6 +173,21 @@ class CursoController extends AbstractController {
     }
   }
 
+  /**
+   * GET -> Obtener toda la información de un curso
+   * ENDPOINT -> curso/especifico
+   * 
+   * PARAMETROS DE REQUEST:
+   * - idCurso
+   * 
+   * RESPONSE:
+   * - idCurso
+   * - nombre
+   * - url
+   * - descripcion
+   * 
+   * 
+   */
   private async getSpecificCurso(req: Request, res: Response) {
     try {
       const curso = await db.Curso.findByPk(req.body.idCurso);
@@ -130,6 +198,18 @@ class CursoController extends AbstractController {
     }
   }
 
+  /**
+   * POST -> Asignar un curso a un agente
+   * ENDPOINT -> curso/asignar
+   * 
+   * PARAMETROS DE REQUEST:
+   * - idUsuario
+   * - idCurso
+   * 
+   * SIN DATOS EN RESPONSE
+   * 
+   * 
+   */
   private async postAsignarCurso(req: Request, res: Response) {
     try {
       const asignacion = await db.UsuarioCurso.create(req.body);
@@ -142,6 +222,18 @@ class CursoController extends AbstractController {
     }
   }
 
+  /**
+   * POST -> Desasignarle un curso a un agente
+   * ENDPOINT -> curso/desasignar
+   * 
+   * PARAMETROS DE REQUEST:
+   * - idAgente
+   * - idCurso
+   * 
+   * SIN DATOS EN RESPONSE
+   * 
+   * 
+   */
   private async postDesasignarCurso(req: Request, res: Response) {
     try {
       const desasignacion = await db.UsuarioCurso.destroy({
@@ -159,6 +251,21 @@ class CursoController extends AbstractController {
     }
   }
 
+  /**
+   * POST -> Modificar asignación de un curso de un agente
+   * ENDPOINT -> curso/modificarAsignacion
+   * 
+   * PARAMETROS DE REQUEST:
+   * - idAgente
+   * - idCurso
+   * - newPrioridad
+   * - newEstado
+   * - newFecha"
+   * 
+   * SIN DATOS EN RESPONSE
+   * 
+   * 
+   */
   private async postModificarAsignacion(req: Request, res: Response) {
     try {
       const newAsignacion = await db.UsuarioCurso.update(
@@ -183,6 +290,7 @@ class CursoController extends AbstractController {
     }
   }
 
+  
   private async postCambiarEstado(req: Request, res: Response) {
     try {
     } catch (err) {}
